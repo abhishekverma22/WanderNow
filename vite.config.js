516150bcee07a6dace0892c7e4ea7fbe9a547338
abhishekverma22 <vermaabhishekabhi22@gmail.com>
2025-11-09 13:47:19 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   optimizeDeps: {
     include: ['react-google-places-autocomplete'],
   },
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 })
