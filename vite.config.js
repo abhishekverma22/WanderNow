edb817a5eb90c75754c3cfe64e3f2bd85929f749
abhishekverma22 <vermaabhishekabhi22@gmail.com>
2025-11-09 20:15:00 +0530

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
   optimizeDeps: {
     include: ['react-google-places-autocomplete'],
   },
   plugins: [react(), tailwindcss()],
   build: {
-    chunkSizeWarningLimit: 1600,
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'firebase-vendor': ['firebase'],
+          'google-vendor': ['@google/generative-ai', '@react-oauth/google'],
+          'maps-vendor': ['@react-google-maps/api'],
+          'ui-vendor': ['framer-motion', 'lucide-react', 'react-icons'],
+        },
+      },
+    },
   },
 })
